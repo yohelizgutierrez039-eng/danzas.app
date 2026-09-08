fd78697f6431301e0249d78e9fc26c980266d5c4
Lizzana Martinez <martinezperezlizzana@gmail.com>
2026-09-08 15:40:20 -0500

    aa

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Sidebar from '../components/sidebar'
+import loading from '<div styleName={} />
+
+import ErrorMensaje from '../components/ErrorMensaje'
+
 function Home 
 () {
     return (
         <>
         <Navbar/>
         <Sidebar/>
-        <Footer/>
+        <Footer/> 
+       
+<Loading></Loading>
         </>
     )
 }
